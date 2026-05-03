10c6ce554db8773979f6bb9cd58009994ed8e945
Vito Leonelli <leonellivito444@gmail.com>
2026-05-03 10:45:48 -0300

    Shape Renderer - Static Shape Counter

--- Projects/Shape_Renderer/Shape.js
+++ Projects/Shape_Renderer/Shape.js
@@ -1,8 +1,13 @@
 export class Shape {
 
+    static totalShapes = 0;
+
     constructor(color) {
         this.color = color;
+        Shape.totalShapes++;
     }
 
     describe() { return `A ${this.color} shape`; }
+
+    static getTotalCreated(){ return Shape.totalShapes; }
 }
--- Projects/Shape_Renderer/Shape.ts
+++ Projects/Shape_Renderer/Shape.ts
@@ -1,10 +1,15 @@
 export class Shape {
 
+    static totalShapes: number = 0;
+
     public color: string;
 
     constructor(color: string) {
         this.color = color;
+        Shape.totalShapes++;
     }
 
     describe(): string { return `A ${this.color} shape`; }
+
+    static getTotalCreated(): number { return Shape.totalShapes; }
 }
